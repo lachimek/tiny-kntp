e2e055cae245e19e66ff0722446ffd0ddf922a50
Michał Woźniak <lachimek1@gmail.com>
2022-06-26 23:35:05 +0200

    naming changes in form

--- components/Form.tsx
+++ components/Form.tsx
@@ -4,11 +4,11 @@ import { zodResolver } from '@hookform/resolvers/zod';
 
 import Loader from './Loader';
 import { trpc } from '../utils/trpc';
-import { schema } from '../shared/form.schema';
+import { formSchema } from '../shared/form.schema';
 
 type Inputs = {
   longUrl: string;
-  optionalEnding: string;
+  customEnding: string;
 };
 
 const Form = () => {
@@ -22,8 +22,8 @@ const Form = () => {
     formState: { errors },
   } = useForm<Inputs>({
     mode: 'onChange',
-    defaultValues: { longUrl: '', optionalEnding: '' },
-    resolver: zodResolver(schema),
+    defaultValues: { longUrl: '', customEnding: '' },
+    resolver: zodResolver(formSchema),
   });
 
   const createTinyLink = trpc.useMutation('create-tiny-link');
@@ -31,42 +31,42 @@ const Form = () => {
 
   const [loading, setLoading] = useState(false);
   const [checked, setChecked] = useState(false);
-  const [available, setAvailable] = useState(true);
+  const [available, setAvailable] = useState(false);
 
   const checkIfAvailable = async () => {
-    const value = getValues('optionalEnding');
+    const value = getValues('customEnding');
+    console.log(value);
+
     setChecked(true);
-    if (value.length < 3 && value.length !== 0) {
-      setError('optionalEnding', {
+    if (value.length < 3) {
+      setError('customEnding', {
         type: 'manual',
         message: 'Minimum 3 characters or empty',
       });
-    } else if (value.length === 0) {
-      clearErrors('optionalEnding');
-    } else {
-      console.log(value);
-      setLoading(true);
-      const response = await checkIfEndingTaken.mutateAsync({
-        optionalEnding: value,
+      return;
+    }
+
+    setLoading(true);
+    const response = await checkIfEndingTaken.mutateAsync({
+      customEnding: value,
+    });
+    clearErrors('customEnding');
+    if (response.ok) {
+      setAvailable(true);
+      setLoading(false);
+    } else if (!response.ok) {
+      setAvailable(false);
+      setLoading(false);
+      setError('customEnding', {
+        type: 'custom',
+        message: 'Ending already taken',
       });
-      clearErrors('optionalEnding');
-      if (response.ok) {
-        setAvailable(true);
-        setLoading(false);
-      } else if (!response.ok) {
-        setAvailable(false);
-        setLoading(false);
-        setError('optionalEnding', {
-          type: 'custom',
-          message: 'Ending already taken',
-        });
-      }
     }
   };
 
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    if (!checked && data.optionalEnding !== '') {
-      setError('optionalEnding', {
+    if (!checked && data.customEnding !== '') {
+      setError('customEnding', {
         type: 'custom',
         message: 'Please check if ending is available',
       });
@@ -106,12 +106,16 @@ const Form = () => {
             className="block text-gray-200 text-md mb-2"
             htmlFor="customEnding"
           >
-            Optional: Your own custom ending [3 to 20 characters]
+            Your own custom ending [3 to 20 characters]
           </label>
           <div
             className={`w-full flex ${
-              errors.optionalEnding && 'border-[1px] border-red-500 rounded-lg'
-            }`}
+              errors.customEnding && 'border-[1px] border-red-500 rounded-lg'
+            }
+            ${
+              available && checked && 'border-[1px] border-green-500 rounded-lg'
+            }
+            `}
           >
             <input
               className="appearance-none rounded-lg rounded-r-none py-2 px-3 w-4/5 text-gray-200 bg-gray-700 leading-tight focus:outline-none focus:shadow-inner"
@@ -119,11 +123,13 @@ const Form = () => {
               id="customEnding"
               maxLength={20}
               placeholder="tiny.kntp.pl/CustomEnding"
-              {...register('optionalEnding')}
+              {...register('customEnding')}
             />
             <button
               className={`bg-gray-700 w-fit md:w-1/5 rounded-r-lg text-gray-200 ${
-                errors.optionalEnding && 'text-red-500'
+                errors.customEnding && 'text-red-500'
+              } ${
+                available && checked && 'text-green-500'
               } hover:text-gray-300 transition-colors border-l-2 border-gray-600 px-2 md:px-0`}
               type="button"
               onClick={() => checkIfAvailable()}
@@ -132,9 +138,9 @@ const Form = () => {
               {loading ? <Loader /> : 'Available?'}
             </button>
           </div>
-          {errors.optionalEnding && (
+          {errors.customEnding && (
             <span className="text-red-500 text-sm">
-              {errors.optionalEnding.message}
+              {errors.customEnding.message}
             </span>
           )}
         </div>
